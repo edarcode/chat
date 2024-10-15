f31624f38d6126b9e50b3b458330045d944c85e4
Edwin Ortiz <edarcode@gmail.com>
2024-10-15 17:41:21 -0500

    refactor

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,17 @@
 import css from "./css.module.css";
 
 export default function Home() {
-  return <div className={css.home}>Home</div>;
+  return (
+    <section className={css.home}>
+      <article className={css.intro}>
+        <h1>Chat</h1>
+        <p>
+          Bienvenido a Chat, la nueva forma de estar en contacto con las
+          personas que más te importan. Envía mensajes de manera rápida y
+          segura, ya sea con tus amigos, familia o compañeros de trabajo, todo
+          desde una sola aplicación.
+        </p>
+      </article>
+    </section>
+  );
 }
