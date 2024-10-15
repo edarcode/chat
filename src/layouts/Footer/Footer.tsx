5315093f00960ac636543a27673920f7a9290155
Edwin Ortiz <edarcode@gmail.com>
2024-10-15 18:32:47 -0500

    agg logout

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,11 +1,18 @@
+import { useAuth } from "../../auth/useAuth";
+import Btn from "../../components/buttons/Btn/Btn";
 import Networks from "../../components/navs/Networks/Networks";
 import css from "./css.module.css";
 
 export default function Footer() {
-	return (
-		<footer className={css.footer}>
-			<Networks />
-			<p>© Casi todos los derechos reservados.</p>
-		</footer>
-	);
+  const token = useAuth((auth) => auth.token);
+  const logout = useAuth((auth) => auth.removeToken);
+  return (
+    <footer className={css.footer}>
+      <Btn onClick={logout} isVisible={!!token}>
+        Cerrar sesión
+      </Btn>
+      <Networks />
+      <p>© Casi todos los derechos reservados.</p>
+    </footer>
+  );
 }
