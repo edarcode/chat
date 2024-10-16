bf7757326248d3e92ca5d1a12f735f7c4cdbb09a
Edwin Ortiz <edarcode@gmail.com>
2024-10-16 14:10:54 -0500

    refactor

--- src/pages/Chat/useGlobalChatState.tsx
+++ src/pages/Chat/useGlobalChatState.tsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
-import { Message } from "./WrapperChat/getChatService";
 import { Account } from "./getAccountService";
+import { Message } from "./WrapperChat/WrapperFollowingTo/FollowingTo/getChatService";
 
 type Chat = {
   chat: Message[] | undefined;
